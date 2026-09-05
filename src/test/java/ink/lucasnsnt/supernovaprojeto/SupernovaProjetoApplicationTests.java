c9acc605d69b1b490c8c0ea9228a32cf0b931c11
Lucas Santos <lucasnsntcontato@outlook.com>
2026-09-05 11:46:03 -0300

    create repository

---
+++ src/test/java/ink/lucasnsnt/supernovaprojeto/SupernovaProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.lucasnsnt.supernovaprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupernovaProjetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
